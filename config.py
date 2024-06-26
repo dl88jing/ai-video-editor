e91b2302edd2a7e6f66a95c5429a76543a5c607f
dl88jing <147234313+dl88jing@users.noreply.github.com>
2024-06-26 03:04:40 -0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
-OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
